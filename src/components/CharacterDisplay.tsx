5d9afbbac5cda13e5b0d30d5a9afb4192e0540ff
mich0w0h <aokimiho71@gmail.com>
2024-02-26 05:45:09 +0900

    fix: preload body image to prevent loading after page shown

--- src/components/CharacterDisplay.tsx
+++ src/components/CharacterDisplay.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CharacterEar from "./CharacterEar";
 import CharacterFace from "./CharacterFace";
 import "./CharacterDisplay.css";
 import CharacterBodyImg from "../assets/character-body.png";
 
 const CharacterDisplay: React.FC = () => {
+  const [loadedBodyImg, setLoadedBodyImg] = useState<string | null>(null);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = CharacterBodyImg;
+    img.onload = () => {
+      console.log("CharacterBodyImage loaded");
+      setLoadedBodyImg(CharacterBodyImg);
+    };
+  }, []);
+
   return (
     <div className="character">
-      <img
-        src={CharacterBodyImg}
-        alt="Character body"
-        className="character-body"
-      />
+      {loadedBodyImg !== null ? (
+        <img
+          src={loadedBodyImg}
+          alt="Character body"
+          className="character-body"
+        />
+      ) : (
+        <div className="character-body"></div>
+      )}
 
       <CharacterEar side="left" />
       <CharacterEar side="right" />
